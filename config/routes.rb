8d90fde8c0267ef8907f9d43b0721f34ee0423de
jesalg <jesal7@gmail.com>
2014-02-27 18:13:29 -0800

    Route fix

--- config/routes.rb
+++ config/routes.rb
@@ -50,9 +50,6 @@ Radd::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  authenticated :user do
-    root :to => 'home#index'
-  end
   root :to => "home#index"
   devise_for :users
   devise_scope :user do
